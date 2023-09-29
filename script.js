59881982507ab4b9c19a7bde1676515b36be7441
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-09-30 00:47:38 +0530

    error occured

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById("search-form");
-    const resultContainer = document.getElementById("result-container");
-
-    form.addEventListener("submit", async function (e) {
-        e.preventDefault();
-
-        // Get user input
-        const sourceCity = document.getElementById("source").value.trim();
-        const destinationCity = document.getElementById("destination").value.trim();
-
-        if (sourceCity === destinationCity) {
-            alert("Source and destination cities cannot be the same.");
-            return;
+    const form = document.getElementById('search-form');
+    const resultContainer = document.getElementById('result-container');
+  
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
+  
+      const sourceCity = document.getElementById('source').value.trim();
+      const destinationCity = document.getElementById('destination').value.trim();
+  
+      if (sourceCity === destinationCity) {
+        alert('Source and destination cities cannot be the same.');
+        return;
+      }
+  
+      try {
+        const response = await fetch(`http://127.0.0.1:3000/search?source=${sourceCity}&destination=${destinationCity}`);
+        const data = await response.json();
+  
+        if (response.ok) {
+          resultContainer.innerHTML = `<p>Destination City: ${data.destinationCity}</p>`;
+        } else {
+          resultContainer.innerHTML = `<p>Error: ${data.error}</p>`;
         }
-
-        // Make request to Node server
-        try {
-            const response = await fetch(`/search?source=${sourceCity}&destination=${destinationCity}`);
-            const data = await response.json();
-      
-            if (response.ok) {
-              resultContainer.innerHTML = `<p>Destination City: ${data.destinationCity}</p>`;
-            } else {
-              resultContainer.innerHTML = `<p>Error: ${data.error}</p>`;
-            }
-          } catch (error) {
-            console.log("Error fetching data from the server:", error);
-            resultContainer.innerHTML = `<p>Error: Internal server error</p>`;
-          }
-
+      } catch (error) {
+        console.error('Error fetching data from the server:', error);
+        resultContainer.innerHTML = '<p>Error: Internal server error</p>';
+      }
     });
-});
+  });
+  
--- server.js
+++ server.js
@@ -1,37 +1,32 @@
 const express = require('express');
-const axios = require('axios');
 const cors = require('cors');
-
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
 
-app.get('/search', async (req, res) => {
+const rawData = fs.readFileSync('cities.json');
+const citiesData = JSON.parse(rawData);
+
+app.get('/search', (req, res) => {
   const { source, destination } = req.query;
 
   if (source === destination) {
     return res.status(400).json({ error: 'Source and destination cities cannot be the same.' });
   }
 
+  const matchingCity = citiesData.find((city) => city.name === source);
+
+  if (!matchingCity) {
+    return res.status(404).json({ error: 'Source city not found in the data.' });
+  }
+
   try {
-    const response = await axios.get('https://gowithgbi.com/api/city');
-    const data = response.data;
-
-    const matchingCities = data.filter((city) => city.name === source);
-    if (matchingCities.length === 0) {
-      return res.status(404).json({ error: 'Source city not found in the API.' });
-    }
-
-    const destinationCity = matchingCities[0].destination;
-    res.json({ destinationCity });
+    const resultCity = matchingCity.state.name;
+    res.json({ destinationCity: resultCity });
   } catch (error) {
-    console.error(error);
+    console.error('Error processing request:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
